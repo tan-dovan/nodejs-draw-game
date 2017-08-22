a9954b283b6511bd32f2ce1d89b101666103f3bb
Dmytro Zubelevych <dz@jelastic.com>
2017-08-22 08:56:45 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+// Based on the tutorial:
+// http://tutorialzine.com/2012/08/nodejs-drawing-game/
+
+//Restrictions on HEROKU:
+// Doesn't support installing dependencies with npm with node 0.8
+// Doesn't support websocekts.
+
+// Including libraries
+
+var app = require('http').createServer(handler),
+	io = require('socket.io').listen(app),
+	nstatic = require('node-static'); // for serving files
+
+// This will make all the files in the current folder
+// accessible from the web
+var fileServer = new nstatic.Server('./');
+	
+// This is the port for our web server.
+// you will need to go to http://localhost:3000 to see it
+var port = process.env.PORT || 8080; // Cloud9 + Heroku || localhost
+app.listen(port);
+
+// If the URL of the socket server is opened in a browser
+function handler (request, response) {
+
+    request.addListener('end', function () {
+        fileServer.serve(request, response); // this will return the correct file
+    });
+    //You need this line for it to work
+        request.resume();
+     }
+
+// Delete this row if you want to see debug messages
+// io.set('log level', 1);
+
+// Heroku doesn't support websockets so...
+// Detect if heroku via config vars
+// https://devcenter.heroku.com/articles/config-vars
+// heroku config:add HEROKU=true --app node-drawing-game
+if (process.env.HEROKU === 'true') {
+    io.configure(function () {
+        io.set("transports", ["xhr-polling"]);
+        io.set("polling duration", 10);
+    });
+}
+
+// Listen for incoming connections from clients
+io.sockets.on('connection', function (socket) {
+
+	// Start listening for mouse move events
+	socket.on('mousemove', function (data) {
+		
+		// This line sends the event (broadcasts it)
+		// to everyone except the originating client.
+		socket.broadcast.emit('moving', data);
+	});
+});
